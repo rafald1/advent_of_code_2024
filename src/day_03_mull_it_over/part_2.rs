1bff23b9c30e1739f13685fb316d645dbedfa11c
rafald1 <116283372+rafald1@users.noreply.github.com>
2024-12-03 15:55:37 +0100

    Add day 3 puzzle solution (#2)

---
+++ src/day_03_mull_it_over/part_2.rs
@@ -0,0 +1,76 @@
+fn get_data(file_path: &str) -> String {
+    std::fs::read_to_string(file_path).expect("Should open a file")
+}
+
+fn process_do_and_dont_operations(corrupted_data: &str) -> Vec<&str> {
+    let mut corrupted_data = corrupted_data;
+    let mut corrupted_data_parts: Vec<&str> = Vec::new();
+
+    while let Some((to_process, rest)) = corrupted_data.split_once("don't()") {
+        corrupted_data_parts.push(to_process);
+        corrupted_data = if let Some((_, rest_after_do)) = rest.split_once("do()") {
+            rest_after_do
+        } else {
+            break;
+        };
+    }
+
+    if !corrupted_data.is_empty() {
+        corrupted_data_parts.push(corrupted_data);
+    }
+
+    corrupted_data_parts
+}
+
+fn find_valid_multiplication_operations(corrupted_data_parts: &[&str]) -> Vec<(u32, u32)> {
+    let mut multiplicands_and_multipliers: Vec<(u32, u32)> = Vec::new();
+
+    for &corrupted_data_part in corrupted_data_parts {
+        let mut corrupted_data = corrupted_data_part;
+
+        while let Some((_, corrupted_data_after_mul)) = corrupted_data.split_once("mul(") {
+            corrupted_data = corrupted_data_after_mul;
+
+            if let Some((numbers, _)) = corrupted_data.split_once(")") {
+                if let Some((multiplicand, multiplier)) = numbers.split_once(",") {
+                    if let (Ok(multiplicand), Ok(multiplier)) =
+                        (multiplicand.parse::<u32>(), multiplier.parse::<u32>())
+                    {
+                        multiplicands_and_multipliers.push((multiplicand, multiplier));
+                    }
+                }
+            }
+        }
+    }
+
+    multiplicands_and_multipliers
+}
+
+fn process_multiplication_operations(multiplication_operations: &[(u32, u32)]) -> u64 {
+    multiplication_operations
+        .iter()
+        .map(|(multiplicand, multiplier)| (*multiplicand * *multiplier) as u64)
+        .sum()
+}
+
+pub(crate) fn solve() -> Result<String, Box<dyn std::error::Error>> {
+    let corrupted_data: String = get_data("./input/03.txt");
+    let corrupted_data: Vec<&str> = process_do_and_dont_operations(&corrupted_data);
+    let multiplications: Vec<(u32, u32)> = find_valid_multiplication_operations(&corrupted_data);
+    let result: u64 = process_multiplication_operations(&multiplications);
+    Ok(format!("Day 3 Mull It Over (Part 2): {}.", result))
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::day_03_mull_it_over::part_2::*;
+
+    #[test]
+    fn solve_with_test_data() {
+        let corrupted_data = get_data("./test_input/03_02.txt");
+        let corrupted_data = process_do_and_dont_operations(&corrupted_data);
+        let multiplications = find_valid_multiplication_operations(&corrupted_data);
+        let result = process_multiplication_operations(&multiplications);
+        assert_eq!(result, 48);
+    }
+}
